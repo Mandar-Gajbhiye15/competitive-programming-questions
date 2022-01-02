e92e0c2213026588649e8b3e7c03ad579f6fdb72
MANDAR DILIP GAJBHIYE <82111449+Mandar-Gajbhiye15@users.noreply.github.com>
2022-01-02 23:53:18 +0530

    added files
    Codechef Problem #FLOW018

---
+++ flow018.py
@@ -0,0 +1,18 @@
+# t = int(input())
+# fact = 1
+# i = 1
+# x = 0
+# list1 = []
+# for _ in range(t):
+#     n = int(input())
+#     if n>0:
+#         fact = fact * n
+#         n-+1
+#         list1.append(fact)
+#     elif n == 0:
+#         list1.append(fact)
+#     else :
+#         list1.append(fact=0)
+# for x in range(len(list1)):
+#     print(list1[x])
+    
